2f08fda41eb2574c9db6fb9d1e35cf16bad5ea9f
Harivansh <hello@harivan.sh>
2020-04-07 12:50:59 +0400

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,31 @@
-import setuptools
+from setuptools import find_packages, setup
 
 
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-setuptools.setup(
+setup(
     name = "harivansh-laravel-docker",
-    version = "0.1.1-rc.1",
-    author = "Harivansh",
-    author_email = "hello@harivan.sh",
+    version = "0.1.1-rc.2",
     description = "A package to automate the installation of Laravel projects on Docker.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = "https://github.com/merchant-prince/laravel-docker",
-    packages = setuptools.find_packages(),
+    author = "Harivansh",
+    author_email = "hello@harivan.sh",
     classifiers = [
-        "Programming Language :: Python :: 3.8",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: POSIX"
     ],
+    keywords = "laravel docker",
+    packages = find_packages(),
     python_requires = '>=3.8',
+    install_requires = [
+        "cryptography",
+        "harivansh-scripting-utilities"
+    ]
 )
