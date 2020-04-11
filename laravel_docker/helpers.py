0d8dc2e752518d223dd72d013878cd2bb693ebf2
Harivansh <hello@harivan.sh>
2020-04-11 18:08:31 +0400

    major refactoring of the helpers module

--- laravel_docker/helpers.py
+++ laravel_docker/helpers.py
@@ -1,10 +1,11 @@
 import os
-import re
 import random
-import string
+import re
 import readline
+import string
 from collections.abc import Mapping
-from scripting_utilities import Print
+
+from harivansh_scripting_utilities.print import success, info, warning, error
 
 
 class Question:
@@ -12,28 +13,25 @@ class Question:
     This class is used to ask questions to the user.
 
     Attributes:
-        question (string):
+        _question (str):
             The question to ask.
             e.g.: "What is your name?"
 
-        validators ([callable]):
-            The validation functions. This is an array of callable objects which
-            raise a ValueError when the validation fails.
+        _validators ([callable]):
+            An array of validation functions which raise ValueErrors on failure.
 
-        default_answer (str):
-            The default answer to the question. This is used if the user enters
-            an EOL as the first character to the question.
+        _default_answer (str):
+            The default answer to the question.
+            This is used if the user enters an EOL as the first character as an answer to the question.
 
-        max_tries (int):
-            The maximum number of times to try a question if its validation
-            fails.
+        _max_tries (int):
+            The maximum number of times to try a question.
 
-        answer (str):
-            The answer to the question as entered by the user.
+        _answer (str):
+            The user's answer to the question.
     """
 
-
-    def __init__(self, question, validators = [lambda a: None], default_answer = None, max_tries = 3):
+    def __init__(self, question, validators=[lambda a: None], default_answer=None, max_tries=3):
         self._question = question
         self._max_tries = max_tries
         self._default_answer = default_answer
@@ -43,10 +41,9 @@ class Question:
             if not callable(validator):
                 raise ValueError("One of the validators provided is not a callable.")
 
-        self.validators = validators
+        self._validators = validators
 
         self._ask()
-
 
     def _ask(self):
         """
@@ -54,29 +51,26 @@ class Question:
         """
 
         for try_count in range(0, self._max_tries):
-            answer = self._ask_question(f"{self._question}: ", self._default_answer)
+            answer = self.format(f"{self._question}: ", self._default_answer)
 
             if answer == '' and self._default_answer is not None:
                 answer = self._default_answer
 
             try:
-                for validator in self.validators:
-                    validator(answer)
+                for validate in self._validators:
+                    validate(answer)
 
                 self._answer = answer
             except ValueError as exception:
-                Print.eol()
-                Print.warning(exception)
-                Print.eol(2)
-
+                print(f"\n{warning(exception)}\n\n", end="")
                 continue
             else:
                 break
         else:
             raise ValueError("You have entered the wrong input too many times.")
 
-
-    def _ask_question(self, question, default_answer):
+    @staticmethod
+    def format(question, default_answer):
         """
         Ask a question with a default answer.
 
@@ -84,6 +78,7 @@ class Question:
             question (str)
             default_answer (str)
         """
+
         readline.set_startup_hook(lambda: readline.insert_text(default_answer))
 
         try:
@@ -91,41 +86,35 @@ class Question:
         finally:
             readline.set_startup_hook()
 
-
     def __str__(self):
         return self._answer
-
-
 
 
 class Validation:
     """
     This class is used to validate answers collected from the users.
-    Each of the validators implemented SHOULD raise a ValueError when the
-    condition of the validator is not met, and they should not return any value.
+    Each of the validators implemented SHOULD raise a ValueError when the condition of the validator is not met,
+    and they should not return any value.
     """
 
-
     @staticmethod
-    def directory_existence(name):
-        if name in os.listdir() and os.path.isdir(name):
+    def directory_exists(name):
+        if os.path.isdir(name):
             raise ValueError("Another directory with the same name already exists in the current directory.")
 
-
     @staticmethod
-    def is_pascalcased(value):
+    def is_pascalcase(value):
         if re.match(r'^[A-Z][a-z]+(?:[A-Z][a-z]+)*$', value) is None:
-            raise ValueError("The provided value is not PascalCased alphabetic string.")
-
+            raise ValueError("The provided value is not a PascalCased alphabetic string.")
 
     @staticmethod
     def is_url(value):
         url_regex = re.compile(
             r'^'
-                r'(?:http|ftp)s?://' # scheme
-                r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?))' # domain
-                r'(?::\d+)?' # port (optional)
-                r'(?:/?|[/?]\S+)' # path
+            r'(?:http|ftp)s?://'  # scheme
+            r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?))'  # domain
+            r'(?::\d+)?'  # port (optional)
+            r'(?:/?|[/?]\S+)'  # path
             r'$',
             re.IGNORECASE
         )
@@ -134,47 +123,41 @@ class Validation:
             raise ValueError("The provided value is not a valid domain.")
 
 
-
-
 class Parser:
     """
-    This is the parser responsible for processing the project configuration
-    templates provided.
+    This is the parser responsible for processing the project configuration templates provided.
 
     Attributes:
-        TEMPLATES_DIRECTORY_PATH (str):
+        Parser.TEMPLATES_DIRECTORY_PATH (str):
             The absolute path to the templates directory.
 
     Properties:
         _raw_template_string (str)
-        _parsed_template_string (str)
+        parsed_template_string (str)
     """
-
 
     TEMPLATES_DIRECTORY_PATH = f"{os.path.dirname(os.path.abspath(__file__))}/templates"
 
-
     def __init__(self):
         self._raw_template_string = None
-        self._parsed_template_string = None
-
-
-    @property
-    def parsed_template_string(self):
-        return self._parsed_template_string
-
+        self.parsed_template_string = None
 
     @staticmethod
-    def template_path(path = ""):
-        if path.startswith("/"):
-            raise ValueError("You can only specify a relative path in this method.")
+    def template_path(path):
+        filepath = f"{Parser.TEMPLATES_DIRECTORY_PATH}/{path.rstrip('/')}"
 
-        return f"{Parser.TEMPLATES_DIRECTORY_PATH}/{path.rstrip('/')}"
+        if not os.path.isfile(filepath):
+            raise ValueError("There is no file at the provided path within the templates directory.")
 
+        return filepath
 
     def read_template(self, template_path):
         """
-        Set the raw template string from the template specified.
+        Set the raw template string from the template file specified.
+
+        Args:
+            template_path (str):
+                The path to the template file.
         """
 
         with open(template_path) as template:
@@ -182,31 +165,30 @@ class Parser:
 
         return self
 
-
     def add_template_string(self, template_string):
         """
         Set the raw template string to the provided one.
+
+        Args:
+            template_string (str):
+                The template string to store for later processing.
         """
 
         self._raw_template_string = template_string
 
         return self
 
-
-    def parse(self,variables = {}, delimiters_creator = lambda variable_name: f"[[{variable_name}]]"):
+    def parse(self, variables={}, delimiters_creator=lambda variable_name: f"[[{variable_name}]]"):
         """
-        Parse the stored template with the provided variables through the
-        delimiters_creator.
+        Parse the stored template with the provided variables through the delimiters_creator.
 
         Args:
             variables (dict):
-                The variables replace in the template. They are passed through
-                the delimiters_creator callable to create a token which is
-                searched and replaced throughout the template.
+                The variables to replace in the template. They are passed through the delimiters_creator callable to
+                create a token which is then searched and replaced throughout the template.
 
             delimiters_creator (callable):
-                The function through which the variable keys are passed to
-                return a token - which is replaced throughout the template.
+                The function through which the variable keys are passed to return a token (see above for description).
         """
 
         if not isinstance(variables, Mapping):
@@ -220,105 +202,88 @@ class Parser:
         for name, value in variables.items():
             parsed_template = parsed_template.replace(delimiters_creator(name), str(value))
 
-        unique_token = "".join(random.choices(f"{string.ascii_uppercase}_", k = 64))
+        unique_token = "".join(random.choices(f"{string.ascii_uppercase}_", k=64))
         # The following regex is created according to the delimiters_creator passed to this method.
-        token_regex = re.compile(r".*" + re.escape(delimiters_creator(unique_token)).replace(unique_token, r"\w[\w_]*") + r".*")
+        token_regex = re.compile(
+            r".*" + re.escape(delimiters_creator(unique_token)).replace(unique_token, r"\w[\w_]*") + r".*"
+        )
 
         if token_regex.match(parsed_template) is not None:
             raise ValueError("There are still unparsed variables in the template.")
 
-        self._parsed_template_string = parsed_template
+        self.parsed_template_string = parsed_template
 
         return self
 
-
-    def output(self, file_path):
+    def output(self, filepath):
         """
         Write the contents of the parsed template string to the specified file.
 
         Args:
-            file_path (str):
-                The path to the file in which the parsed template should be written.
+            filepath (str):
+                The path to the file to which the parsed template should be written.
         """
 
-        if os.path.isfile(file_path):
+        if os.path.isfile(filepath):
             raise ValueError("Another file with the same name already exists.")
 
-        with open(file_path, "w") as file:
-            file.write(self._parsed_template_string)
+        with open(filepath, "w") as file:
+            file.write(self.parsed_template_string)
 
 
-
-
-class PrettyLog:
+def log(message, type="info", position="before"):
     """
-    This class contains decorators to output messages to stdout during the
-    installation process.
+    Outputs a log message before or after running the decorated function
+
+    Args:
+        message (str):
+            The message to print.
+
+        type (str):
+            The type of print function to use. It should be one of the following:
+                ["success", "info", "warning", "error"]
+
+        position (str):
+            Where to print the message; i.e. before or after calling the decorated function.
+            The accepted values are: ["before", "after"]
     """
 
+    if type not in ["success", "info", "warning", "error", "plain"]:
+        raise ValueError("Invalid type provided.")
 
-    @staticmethod
-    def message(message, position = "before", type = "info", eols_before = 1, eols_after = 2):
+    if position not in ["before", "after"]:
+        raise ValueError("Invalid position provided.")
+
+    def print_message():
         """
-        Outputs a message.
-
-        Args:
-            message (str):
-                The message to print.
-
-            type (str):
-                The type of print function to use. It should be one of the
-                following:
-                    ["success", "info", "warning", "error"]
-
-            position (str):
-                Where to print the message; i.e. before or after calling the
-                decorated function. The accepted values are:
-                    ["before", "after"]
-
-            eols_before (int):
-                The number of EOLs to insert before the message.
-
-            eols_after (int):
-                The number of EOLs to insert after the message.
+        Pretty print the message according to the type provided.
         """
 
-        if position not in ["before", "after"]:
-            raise ValueError("Invalid position provided.")
+        if type == "success":
+            prettified_message = success(message)
+        elif type == "info":
+            prettified_message = info(message)
+        elif type == "warning":
+            prettified_message = warning(message)
+        elif type == "error":
+            prettified_message = error(message)
+        else:
+            prettified_message = message
 
-        if type not in ["success", "info", "warning", "error"]:
-            raise ValueError("Invalid type provided.")
+        print(f"\n{prettified_message}\n\n", end="")
 
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            if position == "before":
+                print_message()
 
-        def print_message():
-            print_function = None
+            result = function(*args, **kwargs)
 
-            if type == "success":
-                print_function = Print.success
-            elif type == "info":
-                print_function = Print.info
-            elif type == "warning":
-                print_function = Print.warning
-            elif type == "error":
-                print_function = Print.error
+            if position == "after":
+                print_message()
 
-            Print.eol(eols_before)
-            print_function(message)
-            Print.eol(eols_after)
+            return result
 
+        return wrapper
 
-        def decorator(function):
-            def wrapper(*args, **kwargs):
-                if position == "before":
-                    print_message()
-
-                result = function(*args, **kwargs)
-
-                if position == "after":
-                    print_message()
-
-                return result
-
-            return wrapper
-
-        return decorator
+    return decorator
