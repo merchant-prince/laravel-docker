ccf13dfc02a6eb6a6cf41340daea32730cbb642e
Harivansh <hello@harivan.sh>
2020-04-12 02:13:35 +0400

    remove unnecessary __init__ file, and refactor __main__

--- laravel_docker/__main__.py
+++ laravel_docker/__main__.py
@@ -1,11 +1,9 @@
-from scripting_utilities import Print
-from laravel_docker import Application
+from harivansh_scripting_utilities.print import error
 
+from laravel_docker.application import Application
 
 if __name__ == "__main__":
     try:
         Application().run()
     except Exception as exception:
-        Print.eol()
-        Print.error(exception)
-        Print.eol(2)
+        print(error(f"\n{exception}\n\n"), end="")
